74db0784a0499afa2e6dd7f343ad5a8fbff547df
NaryAina <Aina.Nary@etu.unige.ch>
2014-03-27 15:54:54 +0100

    Premier socket
    connexion client-serveur, pas d'autre communication pour le moment

---
+++ Code/Socket/Socket_Client_test.py
@@ -0,0 +1,4 @@
+import socket
+connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connexion_avec_serveur.connect(('localhost', 12800))
+
---
+++ Code/Socket/Socket_Server_test.py
@@ -0,0 +1,12 @@
+#Socket server qui devra etre implemente sous blender
+
+import socket
+
+connexion_principale = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connexion_principale.bind(('', 12800))
+connexion_principale.listen(5)
+
+# /!\ commande blocante /!\
+connexion_avec_client, infos_connexion = connexion_principale.accept()
+
+print(infos_connexion)
