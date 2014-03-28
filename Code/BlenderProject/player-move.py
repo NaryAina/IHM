7b27f7ff1d3fb614a9680bd7dc568c2686df60de
SamMcSam <ilakebonza@hotmail.com>
2014-03-28 17:43:26 +0100

    Blender prototype

---
+++ Code/BlenderProject/player-move.py
@@ -0,0 +1,29 @@
+import bge   
+
+def main():
+
+    cont = bge.logic.getCurrentController()
+    player = cont.owner
+
+    #la valeur de la propriete passee dans l'actuator
+    vitesse = cont.actuators['speed_actuator']
+    value = player.get(vitesse.propName)
+    
+    keyboard = bge.logic.keyboard
+    JUST_ACTIVATED = bge.logic.KX_INPUT_JUST_ACTIVATED
+    ACTIVE = bge.logic.KX_SENSOR_ACTIVE 
+    
+    if keyboard.events[bge.events.UPARROWKEY] == bge.logic.KX_SENSOR_ACTIVE :
+        value += .002
+    if keyboard.events[bge.events.DOWNARROWKEY] == bge.logic.KX_SENSOR_ACTIVE :
+        value -= .002
+    
+    #update valeur vitesse
+    vitesse.value = str(value)
+    cont.activate(vitesse) 
+         
+    player.applyRotation((0,0,player.get(vitesse.propName)), True)
+    
+    #print(vitesse.value)
+        
+main()
