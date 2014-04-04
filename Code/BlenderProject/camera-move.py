b46f9e4291ad42bafb17c0c591ae8effc3a5dcff
SamMcSam <ilakebonza@hotmail.com>
2014-04-04 11:41:03 +0200

    Camera
    Mouvement de la caméra avec gauche/droite
    note : j'ai désactivé le script des socket!

---
+++ Code/BlenderProject/camera-move.py
@@ -0,0 +1,23 @@
+import bge   
+
+def main():
+
+    cont = bge.logic.getCurrentController()
+    cameraController = cont.owner
+    
+    keyboard = bge.logic.keyboard
+    JUST_ACTIVATED = bge.logic.KX_INPUT_JUST_ACTIVATED
+    ACTIVE = bge.logic.KX_SENSOR_ACTIVE 
+    
+    if keyboard.events[bge.events.LEFTARROWKEY] == bge.logic.KX_SENSOR_ACTIVE :
+        print("ll")
+        cameraController.applyRotation((0,0,-0.02), True)
+    if keyboard.events[bge.events.RIGHTARROWKEY] == bge.logic.KX_SENSOR_ACTIVE :
+        cameraController.applyRotation((0,0,0.02), True)
+    
+    #update valeur vitesse
+    #cont.activate(vitesse) 
+    
+    print('wja')
+        
+main()
