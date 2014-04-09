56543440eb5ab8c76177ae5986432391aecadbbc
NaryAina <Aina.Nary@etu.unige.ch>
2014-04-09 14:52:18 +0200

    serveur-udp

--- Code/Socket/Socket_Server_UDP.py
+++ Code/Socket/Socket_Server_UDP.py
@@ -4,15 +4,6 @@ UDP_IP = "127.0.0.1"
 UDP_PORT = 5005
 cnt = 0
 
-
-
-
-#wait for first msg to collect data about the client
-def first_msg():
-    data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
-    print ("received first message:", data)
-    return addr
-    
 def send_msg(msg,addr):
     MESSAGE = bytes(str(cnt))
     sock.sendto(MESSAGE, addr)
@@ -21,7 +12,7 @@ def send_msg(msg,addr):
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
-#addr = first_msg()
+
 print ("Serveur online")
 while True:
     #wait for a msg
@@ -31,11 +22,6 @@ while True:
     cnt += 1
     print("sending msg")
     send_msg(cnt,addr)
-    
-    #if cnt > 100:
-        #sock.sendto(MESSAGE, addr)
-        #break
-    # MESSAGE = bytes(str(cnt))
-    # sock.sendto(MESSAGE, addr)
+
     
 print ("end server")
