bb3a3f58a31c6adfb22ee6a372e2d8e84e6b43e8
SamMcSam <ilakebonza@hotmail.com>
2014-05-07 11:12:52 +0200

    Update

--- Code/BlenderScript/InitializeChallengeMode.py
+++ Code/BlenderScript/InitializeChallengeMode.py
@@ -5,7 +5,6 @@ from bge import logic as gl
 
 gl.globalDict["liste_mission"] = []
 
-mission = Missions.EvenementWhat([1]) 
+mission = Missions.EvenementWhat([5]) 
 
 gl.globalDict["liste_mission"].append(mission)
-
