7d3035b9b6631b72cb8705ad88c0f6294d65006f
SamMcSam <ilakebonza@hotmail.com>
2014-05-07 12:37:36 +0200

    morning update

--- Code/BlenderScript/InitializeChallengeMode.py
+++ Code/BlenderScript/InitializeChallengeMode.py
@@ -5,6 +5,10 @@ from bge import logic as gl
 
 gl.globalDict["liste_mission"] = []
 
+gl.globalDict["score"] = 0
+
+gl.globalDict["timer"] = 0
+
 mission = Missions.EvenementWhat([5]) 
 
 gl.globalDict["liste_mission"].append(mission)
--- Code/BlenderScript/PlayerMove.py
+++ Code/BlenderScript/PlayerMove.py
@@ -1,34 +1,43 @@
-import bge   
+import bge
+from bge import logic as gl
 import ManageSpeed
 
 def main():
 
-    cont = bge.logic.getCurrentController()
+    cont = gl.getCurrentController()
     player = cont.owner
 
     #la valeur de la propriete passee dans l'actuator
     vitesse = cont.actuators['speed_actuator']
     
-    #value = player.get(vitesse.propName)
-    value = ManageSpeed.computeSpeed(bge.logic.currentGSR)
-        
-    keyboard = bge.logic.keyboard
-    JUST_ACTIVATED = bge.logic.KX_INPUT_JUST_ACTIVATED
-    ACTIVE = bge.logic.KX_SENSOR_ACTIVE 
+    #Recoit et traite valeur du GSR pour la rotation
+    value = ManageSpeed.computeSpeed(gl.currentGSR)
     
+    #Keyboard controlled (Debug)
     """
-    if keyboard.events[bge.events.UPARROWKEY] == bge.logic.KX_SENSOR_ACTIVE :
+    keyboard = gl.keyboard
+    JUST_ACTIVATED = gl.KX_INPUT_JUST_ACTIVATED
+    ACTIVE = gl.KX_SENSOR_ACTIVE 
+    
+    if keyboard.events[bge.events.UPARROWKEY] == gl.KX_SENSOR_ACTIVE :
         value += .002
-    if keyboard.events[bge.events.DOWNARROWKEY] == bge.logic.KX_SENSOR_ACTIVE :
+    if keyboard.events[bge.events.DOWNARROWKEY] == gl.KX_SENSOR_ACTIVE :
         value -= .002
     """
     
-    #update valeur vitesse
+    #update valeur vitesse dans la propriete
     vitesse.value = str(value)
     cont.activate(vitesse) 
          
+    #Rotation
     player.applyRotation((0,0,player.get(vitesse.propName)), True)
-    
-    #print(vitesse.value)
-        
+   
+    #Affichage dans UI
+    scn = gl.getCurrentScene()
+    obl = scn.objects
+    if gl.globalDict["modeChallenge"] :
+        obl["UIspeed"].text = "Speed : " +  str( round(gl.currentGSR, 2))
+    else:
+        obl["UIspeed"].setVisible(False, True)
+             
 main()
